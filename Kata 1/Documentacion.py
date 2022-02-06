44acee94693e1a6744e1e52a93ae499a9c6547d0
Edgarcarrog <edgc21@hotmail.com>
2022-02-06 17:32:19 -0600

    Kata 1 done

---
+++ Kata 1/Documentacion.py
@@ -0,0 +1,11 @@
+sum = 1 + 2 # 3
+product = sum * 2
+print(product)
+
+# Importamos la biblioteca 
+from datetime import date
+print("Today's date is: " + str(date.today()))
+
+print("Bienvenido al programa de bienvenida")
+name = input("Introduzca su nombre ")
+print("Saludos: " + name)
